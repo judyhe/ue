2071e60cfe48d12e8e68518d3797be2631efa0d2
judyhe <judy.w.he@gmail.com>
2012-09-23 21:38:07 -0700

    change spaces in uploads to underscores

--- app/uploaders/data_uploader.rb
+++ app/uploaders/data_uploader.rb
@@ -40,8 +40,8 @@ class DataUploader < CarrierWave::Uploader::Base
   # end
 
   # Override the filename of the uploaded files:
-  # def filename
-  #   "something.jpg" if original_filename
-  # end
+  def filename
+    original_filename.gsub(' ', '_') if original_filename
+  end
 
 end
